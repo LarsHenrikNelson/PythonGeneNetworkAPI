d87f6f23f10ec40b087f0a3f0bef8e8cbe7a1afd
Lars Henrik Nelson <larshnelson@protonmail.com>
2025-02-07 16:40:02 -0500

    Updates to _utils and download_data

--- genenetworkapi/_utils.py
+++ genenetworkapi/_utils.py
@@ -1,3 +1,6 @@
+import io
+from pathlib import Path
+import zipfile
 import tempfile
 from functools import lru_cache
 
@@ -7,9 +10,15 @@ import requests
 
 @lru_cache(maxsize=30)
 def _download(url, filepath: None | str = None):
+    if filepath is not None:
+        filepath = Path(filepath)
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        if filepath is None:
+        url = Path(url)
+        if url.suffix == ".zip":
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall(filepath / f"{url.stem}.zip")
+        elif filepath is None:
             with tempfile.TemporaryFile() as fp:
                 for chunk in r.iter_content(chunk_size=8192):
                     fp.write(chunk)
--- genenetworkapi/v_pre1/download_data.py
+++ genenetworkapi/v_pre1/download_data.py
@@ -1,10 +1,17 @@
+from typing import Literal
+from pathlib import Path
 
 from .query import GN_URL
 from .utils_geno import genofile_location, has_genofile_meta
 from .._utils import _download
 
 
-def download_geno(group, filepath: None | str = None, format: str = "geno"):
+def download_geno(
+    group,
+    filepath: None | Path | str = None,
+    format: Literal["bimbam", "rqtl2", "geno"] = "geno",
+    dataset: str | None = None,
+):
 
     if has_genofile_meta(group):
         # need to check real location of data
@@ -13,16 +20,20 @@ def download_geno(group, filepath: None | str = None, format: str = "geno"):
     if len(vlocation) == 1:
         group = vlocation[0][0:-5]
 
-    geno_url = f"{GN_URL}/genotypes/{group}.{format}"
+    geno_url = f"{GN_URL}/genotypes/{format}/{group}"
+
+    if format == "rqtl2":
+        if dataset is not None:
+            geno_url = geno_url + f"/{dataset}.zip"
 
     return _download(geno_url, filepath)
 
 
-def download_pheno(dataset: str, filepath: None | str = None):
+def download_pheno(dataset: str, filepath: None | Path | str = None):
     pheno_url = f"{GN_URL}/sample_data/{dataset}Publish"
     return _download(pheno_url, filepath)
 
 
-def download_omics(dataset: str, filepath: None | str = None):
+def download_omics(dataset: str, filepath: None | Path | str = None):
     dataset_url = f"{GN_URL}/sample_data/{dataset}"
     return _download(dataset_url, filepath)
